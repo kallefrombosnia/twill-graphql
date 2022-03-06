3e34c1d026f24791c5253936846aecdac78b840a
Izet Mulalić <kallegowild@gmail.com>
2022-03-06 02:02:09 +0100

    more failing tests

---
+++ tests/Utils/Models/Category.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Twill\Graphql\Tests\Utils\Models;
+
+use A17\Twill\Models\Behaviors\HasFiles;
+use A17\Twill\Models\Behaviors\HasMedias;
+use A17\Twill\Models\Behaviors\HasBlocks;
+use A17\Twill\Models\Behaviors\HasRevisions;
+use A17\Twill\Models\Model;
+
+class Category extends Model 
+{
+    use HasFiles, HasMedias, HasBlocks, HasRevisions;
+
+    protected $fillable = [
+        'published',
+        'title',
+        'description',
+    ];
+
+}
--- tests/schema/TestCase.php
+++ tests/schema/TestCase.php
@@ -8,6 +8,18 @@ use Nuwave\Lighthouse\Testing\MakesGraphQLRequests;
 use Nuwave\Lighthouse\Testing\UsesTestSchema;
 use Nuwave\Lighthouse\Testing\MocksResolvers;
 
+// Lighthouse providers
+use Nuwave\Lighthouse\Auth\AuthServiceProvider as LighthouseAuthServiceProvider;
+use Nuwave\Lighthouse\GlobalId\GlobalIdServiceProvider;
+use Nuwave\Lighthouse\LighthouseServiceProvider;
+use Nuwave\Lighthouse\OrderBy\OrderByServiceProvider;
+use Nuwave\Lighthouse\Pagination\PaginationServiceProvider;
+use Nuwave\Lighthouse\Scout\ScoutServiceProvider as LighthouseScoutServiceProvider;
+use Nuwave\Lighthouse\SoftDeletes\SoftDeletesServiceProvider;
+use Nuwave\Lighthouse\Validation\ValidationServiceProvider;
+use Illuminate\Contracts\Config\Repository as ConfigRepository;
+use GraphQL\Error\DebugFlag;
+
 class TestCase extends Orchestra
 {
     use MakesGraphQLRequests;
@@ -22,20 +34,42 @@ class TestCase extends Orchestra
     protected function getPackageProviders($app)
     {
         return [
+
+            // Lighthouse
             GraphqlServiceProvider::class,
+            LighthouseServiceProvider::class,
+            LighthouseAuthServiceProvider::class,
+            GlobalIdServiceProvider::class,
+            LighthouseScoutServiceProvider::class,
+            OrderByServiceProvider::class,
+            PaginationServiceProvider::class,
+            SoftDeletesServiceProvider::class,
+            ValidationServiceProvider::class,
+
+            // Twill
+            \A17\Twill\TwillServiceProvider::class,
         ];
     }
 
     public function getEnvironmentSetUp($app)
     {
-        
-        // Set lighthouse route name
-        config()->set('lighthouse.route.name', 'graphql');
+         /** @var \Illuminate\Contracts\Config\Repository $config */
+        $config = $app->make(ConfigRepository::class);
 
-
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_twill-graphql_table.php.stub';
-        $migration->up();
-        */
+        $config->set('lighthouse.namespaces', [
+            'models' => [
+                'A17\\Twill\\Models',
+                'Twill\\Graphql\\Tests\\Utils\\Models'
+            ]  
+        ]);
+ 
+         $config->set('app.debug', true);
+         $config->set(
+            'lighthouse.debug',
+            DebugFlag::INCLUDE_DEBUG_MESSAGE
+            | DebugFlag::INCLUDE_TRACE
+            // | Debug::RETHROW_INTERNAL_EXCEPTIONS
+            | DebugFlag::RETHROW_UNSAFE_EXCEPTIONS
+         );
     }
 }
